3b5338cdda1396b084c14b985a1b9bd37f85f7f5
Hadi Sharghi <hsharghi@gmail.com>
2021-04-05 01:29:57 +0430

    add setup testing

--- Tests/VaporWalletTests/VaporWalletTests.swift
+++ Tests/VaporWalletTests/VaporWalletTests.swift
@@ -1,15 +1,44 @@
 import XCTVapor
+import Fluent
+import FluentSQLiteDriver
+import Vapor
+
 @testable import VaporWallet
+@testable import App
+
 
 final class VaporWalletTests: XCTestCase {
+    var app: Application!
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
         XCTAssert(true)
     }
-
+    
+    override func setUpWithError() throws {
+        app = try Application.testable()
+    }
+    
+    override func tearDownWithError() throws {
+        app.shutdown()
+    }
+    
+    
+    func setup() {
+//        var env = try Environment.detect()
+//        try LoggingSystem.bootstrap(from: &env)
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try configure(app)
+        try app.run()
+        
+        
+    }
+    
     static var allTests = [
         ("testExample", testExample),
     ]
 }
+
