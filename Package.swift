cadcfb36167cd0e8da0b2594cefd3ba121ada093
Hadi Sharghi <hsharghi@gmail.com>
2021-04-05 01:29:25 +0430

    add sqlite package

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,7 +29,7 @@ let package = Package(
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
-                
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
             ]),
         .testTarget(name: "VaporWalletTests", dependencies: [
             .target(name: "VaporWallet"),
