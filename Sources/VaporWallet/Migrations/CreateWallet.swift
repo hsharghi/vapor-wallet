e65feca8f9f41b3adf9adb7f2c407bec4b489149
Hadi Sharghi <hsharghi@gmail.com>
2021-04-05 19:01:54 +0430

    add foreign key parameter to migration

--- Sources/VaporWallet/Migrations/CreateWallet.swift
+++ Sources/VaporWallet/Migrations/CreateWallet.swift
@@ -1,13 +1,16 @@
 import Fluent
 
 public struct CreateWallet<M:HasWallet>: Migration {
-    public init() { }
+    private var idKey: String
+    public init(foreignKeyColumnName idKey: String = "id") {
+        self.idKey = idKey
+    }
     
     public func prepare(on database: Database) -> EventLoopFuture<Void> {
         return database.schema(Wallet.schema)
             .id()
             .field("name", .string, .required)
-            .field("owner_id", .uuid, .required, .references(M.schema, "id", onDelete: .cascade))
+            .field("owner_id", .uuid, .required, .references(M.schema, .init(stringLiteral: self.idKey), onDelete: .cascade))
             .field("balance", .double, .required)
             .field("decimal_places", .uint8, .required)
             .field("created_at", .datetime, .required)
