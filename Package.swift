fe989207f9e13967af7f491e366af46df79e8d4b
Hadi Sharghi <hsharghi@gmail.com>
2021-04-05 19:03:09 +0430

    add mysql and sqlite packages for testing

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -30,6 +31,7 @@ let package = Package(
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
             ]),
         .testTarget(name: "VaporWalletTests", dependencies: [
             .target(name: "VaporWallet"),
