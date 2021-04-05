703d3600a92a11b7a145221155ccfc802d469fa5
Hadi Sharghi <hsharghi@gmail.com>
2021-04-05 12:42:49 +0430

    fix test setup

--- Tests/VaporWalletTests/VaporWalletTests.swift
+++ Tests/VaporWalletTests/VaporWalletTests.swift
@@ -1,44 +1,26 @@
+import Vapor
+import XCTest
 import XCTVapor
 import Fluent
 import FluentSQLiteDriver
-import Vapor
-
 @testable import VaporWallet
-@testable import App
 
+class VaporWalletTests: XCTestCase {
+    
+    private var app: Application!
+    
+    override func setUp() {
+        super.setUp()
+        
+        app = try! makeApplication()
+        app.databases.use(.sqlite(.memory), as: .sqlite)
+    }
+    
+    private func makeApplication() throws -> Application {
 
-final class VaporWalletTests: XCTestCase {
-    var app: Application!
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssert(true)
-    }
-    
-    override func setUpWithError() throws {
-        app = try Application.testable()
-    }
-    
-    override func tearDownWithError() throws {
-        app.shutdown()
-    }
-    
-    
-    func setup() {
-//        var env = try Environment.detect()
-//        try LoggingSystem.bootstrap(from: &env)
-        let app = Application(.testing)
-        defer { app.shutdown() }
-        try configure(app)
-        try app.run()
         
+        return Application(.testing)
         
+
     }
-    
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
-
