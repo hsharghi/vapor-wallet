e08ac8320d1754e3fef2450f6299a02d58616e8c
Hadi Sharghi <hsharghi@icloud.com>
2021-04-06 20:46:29 +0430

    sum of confirmed transactions only in refreshBalance

--- Sources/VaporWallet/Models/HasWallet.swift
+++ Sources/VaporWallet/Models/HasWallet.swift
@@ -29,87 +29,11 @@ extension HasWallet {
     }
 }
 
-
-
-extension HasWallet {
-    
-    public func createWallet(on db: Database, type name: WalletType) -> EventLoopFuture<Void> {
-        let wallet: Wallet = Wallet(ownerID: self._$idKey.value!, name: name.string)
-        return wallet.save(on: db)
-    }
-    
-    public func createDefaultWallet(on db: Database) throws -> EventLoopFuture<Void> {
-        let wallet: Wallet = Wallet(ownerID: self._$idKey.value!)
-        return wallet.save(on: db)
-    }
-    
-    public func wallets(on db: Database) -> EventLoopFuture<[Wallet]> {
-        Wallet.query(on: db).filter(\.$owner == self._$idKey.value!).all()
-    }
-    
-    public func wallet(on db: Database, type name: WalletType) -> EventLoopFuture<Wallet> {
-        Wallet.query(on: db)
-            .filter(\.$owner == self._$idKey.value!)
-            .filter(\.$name == name.string)
-            .first()
-            .unwrap(or: WalletError.walletNotFound(name: name.string))
-    }
-    
-    public func defaultWallet(on db: Database) -> EventLoopFuture<Wallet> {
-        wallet(on: db, type: .default)
-    }
-    
-}
-
-extension HasWallet {
-    
-    public func walletBalance(on db: Database, type name: WalletType = .default) -> EventLoopFuture<Double> {
-        self.wallet(on: db, type: name).map { $0.balance }
-    }
-    
-    public func canWithdraw(on db: Database, from: WalletType = .default, amount: Double) -> EventLoopFuture<Bool> {
-        self.wallet(on: db, type: from).flatMap { $0.refreshBalance(on: db).map { $0 >= amount } }
-    }
-    
-    public func deposit(on db: Database, to: WalletType = .default, amount: Double, confirmed: Bool, meta: [String: String]? = nil) throws -> EventLoopFuture<Void> {
-        self.wallet(on: db, type: to).flatMap { wallet -> EventLoopFuture<Void> in
-            return db.transaction { database -> EventLoopFuture<Void> in
-                do {
-                    return WalletTransaction(walletID: try wallet.requireID(), type: .deposit, amount: amount, confirmed: confirmed, meta: meta)
-                        .save(on: database)
-                } catch {
-                    return database.eventLoop.makeFailedFuture(WalletError.walletNotFound(name: to.string))
-                }
-            }
-        }
-    }
-    
-    public func withdraw(on db: Database, from: WalletType = .default, amount: Double, meta: [String: String]? = nil) -> EventLoopFuture<Void> {
-        
-        canWithdraw(on: db, from: from, amount: amount)
-            .guard({ $0 == true }, else: WalletError.insufficientBalance)
-            .flatMap { _ in
-                self.wallet(on: db, type: from).flatMap { wallet -> EventLoopFuture<Void> in
-                    return db.transaction { database -> EventLoopFuture<Void> in
-                        do {
-                            return WalletTransaction(walletID: try wallet.requireID(), type: .withdraw, amount: -1 * amount, meta: meta)
-                                .save(on: database)
-                        } catch {
-                            return database.eventLoop.makeFailedFuture(WalletError.walletNotFound(name: from.string))
-                        }
-                    }
-                }
-            }
-    }
-    
-}
-
-
-
 extension Wallet {
     public func refreshBalance(on db: Database) -> EventLoopFuture<Double> {
         self.$transactions
             .query(on: db)
+            .filter(\.$confirmed == true)
             .sum(\.$amount)
             .unwrap(orReplace: 0)
             .flatMap { (balance) -> EventLoopFuture<Double> in
@@ -120,3 +44,4 @@ extension Wallet {
             }
     }
 }
+
