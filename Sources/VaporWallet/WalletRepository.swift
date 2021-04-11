b1e1a52ee4872eca495830a9d94bb4d443bbf969
Hadi Sharghi <hsharghi@icloud.com>
2021-04-11 19:51:34 +0430

    WalletRepository class

---
+++ Sources/VaporWallet/WalletRepository.swift
@@ -0,0 +1,128 @@
+//
+//  File.swift
+//  
+//
+//  Created by Hadi Sharghi on 4/11/21.
+//
+
+import Vapor
+import Fluent
+
+
+public class WalletsRepository<M:HasWallet> {
+    init(db: Database, idKey: M.ID<UUID>) {
+        guard let id = idKey.value else {
+            fatalError("Unsaved models can't have wallets")
+        }
+        self.db = db
+        self.id = id
+    }
+
+    private var db: Database
+    private var id: M.ID<UUID>.Value
+
+    public func create(type name: WalletType = .default) -> EventLoopFuture<Wallet> {
+        let wallet: Wallet = Wallet(ownerID: self.id, name: name.value)
+        return wallet.save(on: db).map { wallet }
+    }
+    
+    public func all() -> EventLoopFuture<[Wallet]> {
+        Wallet.query(on: self.db)
+            .filter(\.$owner == self.id)
+            .all()
+    }
+    
+    public func get(type name: WalletType) -> EventLoopFuture<Wallet> {
+        Wallet.query(on: db)
+            .filter(\.$owner == self.id)
+            .filter(\.$name == name.value)
+            .first()
+            .unwrap(or: WalletError.walletNotFound(name: name.value))
+    }
+    
+    public func `default`() -> EventLoopFuture<Wallet> {
+        get(type: .default)
+    }
+    
+    public func balance(type name: WalletType = .default, withUnconfirmed: Bool = false) -> EventLoopFuture<Double> {
+        if withUnconfirmed {
+            return get(type: name).flatMap { wallet  in
+                wallet.$transactions
+                    .query(on: self.db)
+                    .sum(\.$amount)
+                    .unwrap(orReplace: 0)
+            }
+        }
+        return get(type: name).map { $0.balance }
+    }
+
+    public func canWithdraw(from: WalletType = .default, amount: Double) -> EventLoopFuture<Bool> {
+        get(type: from).flatMap { $0.refreshBalance(on: self.db).map { $0 >= amount } }
+    }
+    
+    public func dep() -> EventLoopFuture<Void> {
+        return self.default().flatMap { wallet -> EventLoopFuture<Void> in
+            let tr = WalletTransaction(walletID: wallet.id!, type: .deposit, amount: 100)
+            return wallet.$transactions.create(tr, on: self.db)
+        }
+    }
+    
+    public func deposit(to: WalletType = .default, amount: Double, confirmed: Bool = true, meta: [String: String]? = nil) throws -> EventLoopFuture<Void> {
+        get(type: to).flatMap { wallet -> EventLoopFuture<Void> in
+            return self.db.transaction { database -> EventLoopFuture<Void> in
+                do {
+                    return WalletTransaction(walletID: try wallet.requireID(), type: .deposit, amount: amount, confirmed: confirmed, meta: meta)
+                        .save(on: database)
+                } catch {
+                    return self.db.eventLoop.makeFailedFuture(WalletError.walletNotFound(name: to.value))
+                }
+            }
+        }
+    }
+    
+
+    public func withdraw(from: WalletType = .default, amount: Double, meta: [String: String]? = nil) -> EventLoopFuture<Void> {
+        
+        canWithdraw(from: from, amount: amount)
+            .guard({ $0 == true }, else: WalletError.insufficientBalance)
+            .flatMap { _ in
+                self.get(type: from).flatMap { wallet -> EventLoopFuture<Void> in
+                    return self.db.transaction { database -> EventLoopFuture<Void> in
+                        do {
+                            return WalletTransaction(walletID: try wallet.requireID(), type: .withdraw, amount: -1 * amount, meta: meta)
+                                .save(on: database)
+                        } catch {
+                            return database.eventLoop.makeFailedFuture(WalletError.walletNotFound(name: from.value))
+                        }
+                    }
+                }
+            }
+    }
+    
+    
+    public func transactions(type name: WalletType = .default,
+                                        paginate: PageRequest = .init(page: 1, per: 10),
+                                        sortOrder: DatabaseQuery.Sort.Direction = .descending) -> EventLoopFuture<Page<WalletTransaction>> {
+        return self.get(type: name).flatMap {
+            $0.$transactions
+                .query(on: self.db)
+                .sort(\.$createdAt, sortOrder)
+                .filter(\.$confirmed == true)
+                .paginate(paginate)
+        }
+    }
+
+    
+    public func unconfirmedTransactions(type name: WalletType = .default,
+                                        paginate: PageRequest = .init(page: 1, per: 10),
+                                        sortOrder: DatabaseQuery.Sort.Direction = .descending) -> EventLoopFuture<Page<WalletTransaction>> {
+        return self.get(type: name).flatMap {
+            $0.$transactions
+                .query(on: self.db)
+                .sort(\.$createdAt, sortOrder)
+                .filter(\.$confirmed == false)
+                .paginate(paginate)
+        }
+    }
+
+}
