ff934e55c861885e075fcbf0a89661a4e8a69119
Hadi Sharghi <hsharghi@gmail.com>
2023-06-14 19:31:36 +0330

    async migration for transactions table

--- Sources/VaporWallet/Migrations/CreateWalletTransaction.swift
+++ Sources/VaporWallet/Migrations/CreateWalletTransaction.swift
@@ -4,37 +4,43 @@ public struct CreateWalletTransaction: Migration {
     public init() { }
     
     public func prepare(on database: Database) -> EventLoopFuture<Void> {
-        return database.enum("type").case("deposit").case("withdraw").create().flatMap { transactionType in
-            return database.schema(WalletTransaction.schema)
-                .id()
-                .field("wallet_id", .uuid, .required, .references(Wallet.schema, "id", onDelete: .cascade))
-                .field("type", transactionType, .required)
-                .field("amount", .int, .required)
-                .field("confirmed", .bool, .required)
-                .field("meta", .json)
-                .field("created_at", .datetime, .required)
-                .field("updated_at", .datetime, .required)
-                .create()
-        }
+        return database.enum("type")
+            .case("deposit")
+            .case("withdraw")
+            .create().flatMap { transactionType in
+                return database.schema(WalletTransaction.schema)
+                    .id()
+                    .field("wallet_id", .uuid, .required, .references(Wallet.schema, "id", onDelete: .cascade))
+                    .field("type", transactionType, .required)
+                    .field("amount", .int, .required)
+                    .field("confirmed", .bool, .required)
+                    .field("meta", .json)
+                    .field("created_at", .datetime, .required)
+                    .field("updated_at", .datetime, .required)
+                    .create()
+            }
     }
     
     public func revert(on database: Database) -> EventLoopFuture<Void> {
-        return database.enum("type").deleteCase("deposit").deleteCase("withdraw").update().flatMap { _ in
-            return database.schema(WalletTransaction.schema).delete()
-        }
+        return database.enum("type")
+            .deleteCase("deposit")
+            .deleteCase("withdraw")
+            .update().flatMap { _ in
+                return database.schema(WalletTransaction.schema).delete()
+            }
         
     }
 }
 
 public struct CreateWalletTransactionAsync: AsyncMigration {
     public init() { }
-
+    
     public func prepare(on database: Database) async throws {
         let transactionType = try await database.enum("type")
             .case("deposit")
             .case("withdraw")
             .create()
-
+        
         try await database.schema(WalletTransaction.schema)
             .id()
             .field("wallet_id", .uuid, .required, .references(Wallet.schema, "id", onDelete: .cascade))
@@ -46,8 +52,8 @@ public struct CreateWalletTransactionAsync: AsyncMigration {
             .field("updated_at", .datetime, .required)
             .create()
     }
-
-
+    
+    
     public func revert(on database: Database) async throws {
         let _ =  try await database.enum("type")
             .deleteCase("deposit")
@@ -55,5 +61,5 @@ public struct CreateWalletTransactionAsync: AsyncMigration {
             .update()
         try await database.schema(WalletTransaction.schema).delete()
     }
-
+    
 }
