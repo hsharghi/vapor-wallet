58b8f1f957f438375b3fea402a22a2f4a47a4bd3
Hadi Sharghi <hsharghi@gmail.com>
2023-06-20 13:58:36 +0330

    add test methods

--- Tests/VaporWalletTests/XCTestManifests.swift
+++ Tests/VaporWalletTests/XCTestManifests.swift
@@ -1,9 +1,33 @@
 import XCTest
 
+extension VaporWalletTests {
+    static let allTests = [
+        ("testAddUser", testAddUser),
+        ("testAddGame", testAddGame),
+        ("testUserHasNoDefaultWallet", testUserHasNoDefaultWallet),
+        ("testUserHasDefaultWallet", testUserHasDefaultWallet),
+        ("testCreateWallet", testCreateWallet),
+        ("testWalletDeposit", testWalletDeposit),
+        ("testWalletTransactionMiddleware", testWalletTransactionMiddleware),
+        ("testWalletWithdraw", testWalletWithdraw),
+        ("testWalletCanWithdraw", testWalletCanWithdraw),
+        ("testWalletCanWithdrawWithMinAllowedBalance", testWalletCanWithdrawWithMinAllowedBalance),
+        ("testMultiWallet", testMultiWallet),
+        ("testTransactionMetadata", testTransactionMetadata),
+        ("testWalletDecimalBalance", testWalletDecimalBalance),
+        ("testConfirmTransaction", testConfirmTransaction),
+        ("testConfirmAllTransactionsOfWallet", testConfirmAllTransactionsOfWallet),
+        ("testTransferBetweenAUsersWallets", testTransferBetweenAUsersWallets),
+        ("testTransferBetweenTwoUsersWallets", testTransferBetweenTwoUsersWallets),
+        ("testMultiModelWallet", testMultiModelWallet),
+        ("testMultiModelWalletTransfer", testMultiModelWalletTransfer),
+    ]
+}
+
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(VaporWalletTests.allTests),
-    ]
+        return [
+            testCase(VaporWalletTests.allTests),
+        ]
 }
 #endif
