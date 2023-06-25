e79cf5f6a8654dbd0a8b7e33f1cb51840bb17477
Hadi Sharghi <hsharghi@gmail.com>
2023-06-25 20:09:40 +0330

    update packages

--- Package.swift
+++ Package.swift
@@ -23,6 +23,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.7.2"),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
