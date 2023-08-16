0e20540ced205f68f59cedb96d00ef9fe80d1bd8
Hadi Sharghi <hsharghi@gmail.com>
2023-08-16 15:57:40 +0330

    add wallet parameters to CreateWalletMiddleware

--- Sources/VaporWallet/Middlewares/WalletModelMiddleware.swift
+++ Sources/VaporWallet/Middlewares/WalletModelMiddleware.swift
@@ -11,13 +11,20 @@ import Fluent
 
 
 public struct WalletMiddleware<M:HasWallet>: AsyncModelMiddleware {
+    private var walletType: WalletType
+    private var decimalPlaces: UInt8
+    private var minAllowedBalance: Int
     
-    public init() {}
+    public init(walletType: WalletType = .default, decimalPlaces: UInt8 = 0, minAllowedBalace: Int = 0) {
+        self.walletType = walletType
+        self.decimalPlaces = decimalPlaces
+        self.minAllowedBalance = minAllowedBalace
+    }
 
     public func create(model: M, on db: Database, next: AnyAsyncModelResponder) async throws {
         try await next.create(model, on: db)
         db.logger.log(level: .info, "default wallet for user \(model.id!) has been created")
         let repo = model.walletsRepository(on: db)
-        try await repo.create()
+        try await repo.create(type: walletType, decimalPlaces: decimalPlaces, minAllowedBalance: minAllowedBalance)
     }
 }
